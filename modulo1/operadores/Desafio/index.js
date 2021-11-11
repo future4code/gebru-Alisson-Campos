9b379963ca23a64f2b1d67560ff800b292d4e33d
AlissonFCampos <alissonferreiracampos@hotmail.com>
2021-11-11 19:39:48 -0300

    Desafio Operadores

---
+++ modulo1/operadores/Desafio/index.js
@@ -0,0 +1,47 @@
+/*let conversorCelsiusParaFahrenheit =Number(prompt("Digite os graus Celsius para a conversão em Fahrenheit"))
+let conversorFahrenheitParaCelsius = Number(prompt("Digite os graus Farenheit para a conversão em Celcius"))
+let conversorFahrennheitParaKelvin = Number(prompt("Digite os graus Farenheit para a conversão em Kelvin"))
+let conversorCelsiusParaKelvin = Number(prompt("Digite os graus Celsius para a conversão em Kelvin"))
+console.log("O resultado é",conversorCelsiusParaFahrenheit * (9/5)+32)
+console.log("O resultado é",(conversorFahrenheitParaCelsius -32) * (5/9))
+console.log("O resultado é",(conversorFahrennheitParaKelvin -32) * 5/9+ 273.15)
+console.log("O resultado é ",conversorCelsiusParaKelvin + 273,14)*/
+
+let Valorquilowatt = 0.05
+let consumoEnergia = 280
+let valorTotal = consumoEnergia * Valorquilowatt
+let desconto = 0.15
+console.log ("Gastei esse mês R$",valorTotal)
+console.log("Valor com Descont0", valorTotal * desconto - valorTotal)
+
+
+let lb = 20
+let conversaoLibrasQuilo = lb /2.205
+console.log("20 libras equivalem a ",conversaoLibrasQuilo,"Kg")
+
+let oz = 10.5
+let conversaoOncaQuilo = oz/3.527
+console.log("10.05 oz equivalem a ",conversaoOncaQuilo, "Kg")
+
+let mi = 100
+let conversaoMilhaMetros = mi* 1609
+console.log("100mi equivalem a" ,conversaoMilhaMetros,"Metros")
+
+let ft = 50
+let conversaoPesMetros = ft/3.281
+console.log("50 ft equivalem a",conversaoPesMetros,"metros")
+
+let gal = 103.56
+let conversaoGalaoLitros = gal*3.785
+console.log("103.56gal equivalem a",conversaoGalaoLitros,"litros")
+
+let xicara = Number(prompt("Insira a quantidade de Xícaras"))
+console.log("Valor da Xícara Convertida para litro é ?",xicara * 3.52,"Litros")
+
+
+
+
+
+
+
+

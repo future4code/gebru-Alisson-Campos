fde624d845a77afe5f91ee59765c8b1d678196a8
AlissonFCampos <alissonferreiracampos@hotmail.com>
2021-11-23 22:29:49 -0300

    Exercício objeto

---
+++ modulo1/objetos/index.js
@@ -0,0 +1,17 @@
+// exercício interpretação 
+//  1- "Matheus Nachtergaele",	"Virginia Cavendish","Canal Brasil"
+//  2-nome: "Juca", 
+//  idade: 3, 
+//  raca: "SRD"
+
+//  nome: "Juba", 
+// 	idade: 3, 
+// 	raca: "SRD"
+
+//     nome: "Jubo", 
+// 	idade: 3, 
+// 	raca: "SRD"
+//      a sintaxe se chama spread ela copia os elementos do objeto podendo ser incluídos,excluídos ou alterados
+
+// //      3- false e no outro console creio ter retornardo null por não ter essa propriedade no objeto
+// Cheguei de viagem agora a pouco e vou terminar os exercícios amanha cedo

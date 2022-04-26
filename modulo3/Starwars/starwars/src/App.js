1643d5dc715ac0c833b4d3b22f342f7259588750
AlissonFCampos <alissonferreiracampos@hotmail.com>
2022-04-26 18:05:55 -0300

    app-starwars

---
+++ modulo3/Starwars/starwars/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import CharacterListPage from "./CharacterListPage/CharacterListPage";
+
+
+const AppStarWars = ()=>{
+return (
+  <div>
+    <h1>Star Wars</h1>
+    <CharacterListPage/>
+  </div>
+)
+}
+
+export default AppStarWars;

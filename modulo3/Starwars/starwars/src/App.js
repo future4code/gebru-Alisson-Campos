3a36d289739906ed9d0f4eddd215028f2dce293b
AlissonFCampos <alissonferreiracampos@hotmail.com>
2022-04-28 19:41:03 -0300

    segundaParte-projeto

--- modulo3/Starwars/starwars/src/App.js
+++ modulo3/Starwars/starwars/src/App.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import CharacterListPage from "./CharacterListPage/CharacterListPage";
+import CharacterDetailPage from "./CharacterDetailPage/CharacterDetailPage";
 
+const AppStarWars = () => {
+  const [currentPage, setCurrentPage] = useState("list")
 
-const AppStarWars = ()=>{
-return (
-  <div>
-    <h1>Star Wars</h1>
-    <CharacterListPage/>
-  </div>
-)
+  const selectPage = () => {
+    if (currentPage === "list") {
+      return <CharacterListPage />
+    }
+    else {
+      return <CharacterDetailPage />
+    }
+  }
+  return (
+    <div>
+      {selectPage()}
+    </div>
+  )
 }
 
 export default AppStarWars;
--- modulo3/Starwars/starwars/src/CharacterListPage/CharacterListPage.js
+++ modulo3/Starwars/starwars/src/CharacterListPage/CharacterListPage.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios"
+import BASE_URL from "../constants/constants";
+import CharacterCard from "./CharacterListPageStyles";
 
-const CharacterListPage = () =>{
+const CharacterListPage = () => {
+    const [characterList, setCharacterList] = useState([])
+    useEffect(() => {
+        getCharacterList()
+    }, [])
+    const getCharacterList = () => {
+        axios.get(`${BASE_URL}/people/`)
+            .then((response) => setCharacterList(response.data.results))
+            .catch((error) => console.log("Erro", error.data))
+    }
+    const showCharacters = () => {
+        return characterList.map((character, index) => {
+            return <CharacterCard key={index}>{character.name}</CharacterCard>
+        })
+    }
+
     return (
-<div>
-   <h5>teste componente</h5>
-</div>
+        <div>
+            <h1>Lista de Personagens</h1>
+            {showCharacters()}
+        </div>
     )
-} 
+}
 export default CharacterListPage

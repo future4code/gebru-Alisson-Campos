11f443760b3aa6f31e02fc4f8e9a65145298e8f0
AlissonFCampos <alissonferreiracampos@hotmail.com>
2022-05-05 14:58:57 -0300

    parte4

--- modulo3/Starwars/starwars/src/CharacterDetailPage/CharacterDetailPage.js
+++ modulo3/Starwars/starwars/src/CharacterDetailPage/CharacterDetailPage.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getCharacterDetails, getPlanetDetails } from "../services/requests";
 
-const CharacterDetailPage = (props) =>{
+const CharacterDetailPage = (props) => {
+    const [details, setDetails] = useState({})
+    const [planet, setPlanet] = useState({})
+
+    useEffect(() => {
+        getCharacterDetails(props.url,setDetails)
+    }, [props.url])
+
+    useEffect(() => {
+        getPlanetDetails(details.homeworld,setPlanet)
+    }, [details.homeworld])
+
+
     return (
-<div>
-   <h1>Detalhes do Personagens</h1>
-   <p>{props.url}</p>
-   <button onClick={props.goToListPage}> Voltar para a lista de Personagens</button>
-</div>
+        <div>
+            <h1>Detalhes do Personagens</h1>
+            {details.name && planet.name ?
+            <div>
+              <p>Nome: {details.name}</p>
+            <p>Planeta de Origem: {planet.name}</p>
+            </div> : <p>Aguarde um momento</p>}
+            
+            <button onClick={props.goToListPage}> Voltar para a lista de Personagens</button>
+        </div>
     )
-} 
+}
 export default CharacterDetailPage
--- modulo3/Starwars/starwars/src/CharacterListPage/CharacterListPage.js
+++ modulo3/Starwars/starwars/src/CharacterListPage/CharacterListPage.js
@@ -1,22 +1,16 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios"
-import BASE_URL from "../constants/constants";
 import CharacterCard from "./CharacterListPageStyles";
-
+import {getCharacterList} from "../services/requests"
 
 const CharacterListPage = (props) => {
     const [characterList, setCharacterList] = useState([])
     useEffect(() => {
-        getCharacterList()
+        getCharacterList(setCharacterList)
     }, [])
-    const getCharacterList = () => {
-        axios.get(`${BASE_URL}/people/`)
-            .then((response) => setCharacterList(response.data.results))
-            .catch((error) => console.log("Erro", error.data))
-    }
+
     const showCharacters = () => {
         return characterList.map((character, index) => {
-            return <CharacterCard onClick={ () =>props.goToDetailsPage(character.url)} key={index}>{character.name}</CharacterCard>
+            return <CharacterCard onClick={() => props.goToDetailsPage(character.url)} key={index}>{character.name}</CharacterCard>
         })
     }
 
